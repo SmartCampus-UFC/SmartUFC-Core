f943164e79d30e090ddca5a46287980d25179f45
Michel Bonfim <michelsb@gmail.com>
2022-08-09 08:21:46 -0300

    first commit

---
+++ data/provision-api.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+#  curl commands to reload the data from the previous tutorial
+#
+#
+
+set -e
+
+printf "Provisioning APIs"
+
+#
+# Create services
+#
+
+curl -s -o /dev/null -X POST --url http://kong:8001/services/ --data 'name=smart-quixada-service' --data 'url=http://10.0.200.100:1026'
+curl -s -o /dev/null -X POST --url http://kong:8001/services/ --data 'name=smart-pici-service' --data 'url=http://10.0.10.67:1026'
+
+#
+# Create routes
+#
+
+curl -s -o /dev/null -X POST --url http://kong:8001/services/smart-quixada-service/routes --data 'paths[]=/campusquixada' --data 'strip_path=true' --data 'methods[]=GET' --data 'methods[]=POST' --data 'methods[]=PUT' --data 'methods[]=PATCH' --data 'methods[]=DELETE' --data 'methods[]=OPTIONS'
+curl -s -o /dev/null -X POST --url http://kong:8001/services/smart-pici-service/routes --data 'paths[]=/campuspici' --data 'strip_path=true' --data 'methods[]=GET' --data 'methods[]=POST' --data 'methods[]=PUT' --data 'methods[]=PATCH' --data 'methods[]=DELETE' --data 'methods[]=OPTIONS'
+
+#
+# Configure CORS per service
+#
+
+curl -s -o /dev/null -X POST http://kong:8001/services/smart-quixada-service/plugins \
+    --data "name=cors"  \
+    --data "config.origins=*" \
+    --data "config.methods=GET" \
+    --data "config.methods=POST" \
+    --data "config.methods=PUT" \
+    --data "config.methods=PATCH" \
+    --data "config.methods=DELETE" \
+    --data "config.methods=OPTIONS" \
+    --data "config.headers=Accept" \
+    --data "config.headers=Accept-Version" \
+    --data "config.headers=Content-Length" \
+    --data "config.headers=Content-MD5" \
+    --data "config.headers=Content-Type" \
+    --data "config.headers=Host" \
+    --data "config.headers=Date" \
+    --data "config.headers=fiware-service" \
+    --data "config.headers=fiware-servicepath" \
+    --data "config.headers=X-Auth-Token" \
+    --data "config.exposed_headers=X-Auth-Token" \
+    --data "config.credentials=true" \
+    --data "config.max_age=3600"
+
+curl -s -o /dev/null -X POST http://kong:8001/services/smart-pici-service/plugins \
+    --data "name=cors"  \
+    --data "config.origins=*" \
+    --data "config.methods=GET" \
+    --data "config.methods=POST" \
+    --data "config.methods=PUT" \
+    --data "config.methods=PATCH" \
+    --data "config.methods=DELETE" \
+    --data "config.methods=OPTIONS" \
+    --data "config.headers=Accept" \
+    --data "config.headers=Accept-Version" \
+    --data "config.headers=Content-Length" \
+    --data "config.headers=Content-MD5" \
+    --data "config.headers=Content-Type" \
+    --data "config.headers=Host" \
+    --data "config.headers=Date" \
+    --data "config.headers=fiware-service" \
+    --data "config.headers=fiware-servicepath" \
+    --data "config.headers=X-Auth-Token" \
+    --data "config.exposed_headers=X-Auth-Token" \
+    --data "config.credentials=true" \
+    --data "config.max_age=3600"
---
+++ services.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+#  Command Line Interface to start all services associated with the Smart Campus UFC
+#
+
+set -e
+export USER_ID=$(id -u)
+dockerCmd="docker-compose"
+
+if (( $# < 1 )); then
+    echo "Illegal number of parameters"
+    echo "usage: services [create|start|stop|help]"
+    exit 1
+fi
+
+stoppingContainers () {
+	echo "Stopping running containers"
+	${dockerCmd} -f docker-compose.yml down -v --remove-orphans
+}
+
+loadData () {
+	waitForDatabase
+	waitForKong	
+	docker run --rm -v $(pwd)/data/provision-api.sh:/provision-api.sh \
+		--network smartufc_main-net \
+		--entrypoint /bin/ash curlimages/curl provision-api.sh
+	echo ""
+}
+
+waitForDatabase () {
+	echo -e "\n Waiting for \033[1mPostgres\033[0m to be available\n"
+	while ! [ `docker inspect --format='{{.State.Health.Status}}' core-db` == "healthy" ]
+	do 
+		sleep 1
+	done
+}
+
+waitForKong () {
+	echo -e "\n Waiting for \033[1mKong\033[0m to be available\n"
+	while ! [ `docker inspect --format='{{.State.Health.Status}}' core-kong` == "healthy" ]
+	do 
+		sleep 1
+	done
+}
+
+command="$1"
+case "${command}" in
+	"help")
+        echo "usage: services [create|start|stop]"    	 
+		;;
+	"stop")
+		export $(cat .env | grep "#" -v)
+		stoppingContainers
+		;;
+	"start")
+		export $(cat .env | grep "#" -v)
+		stoppingContainers
+		echo -e "Starting containers"
+		echo ""
+		COMPOSE_PROFILES=database KONG_DATABASE=postgres ${dockerCmd} -f docker-compose.yml up -d
+		loadData
+		;;
+	"create")
+		export $(cat .env | grep "#" -v)
+		echo "Pulling Docker images"
+		docker pull curlimages/curl
+		${dockerCmd} -f docker-compose.yml pull
+		;;
+	*)
+		echo "Command not Found."
+		echo "usage: services [create|start|stop|help]"
+		exit 127;
+		;;
+esac
